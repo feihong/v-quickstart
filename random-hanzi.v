72259a78a34872142589ed06ba4ef339b2628303
Feihong Hsu <feihong.hsu@gmail.com>
2021-05-09 10:41:11 -0500

    Define rand_hanzi_array

--- random-hanzi.v
+++ random-hanzi.v
@@ -1,11 +1,16 @@
-import rand
+import rand { u32_in_range }
 
 fn rand_hanzi() string {
-	n := rand.u32_in_range(0x4e00, 0x9fff + 1)
+	n := u32_in_range(0x4e00, 0x9fff + 1)
   return utf32_to_str(n)
 }
 
-for _ in 0 .. 10 {
-  print(rand_hanzi())
+fn rand_hanzi_array(n int) []string {
+	mut res := []string{len: n}
+	for i in 0 .. n {
+		res[i] = rand_hanzi()
+	}
+	return res
 }
-println('')
+
+println(rand_hanzi_array(8).join(' '))
